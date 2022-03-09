7cb23b9316245c320b0a324975b360e65d5b501b
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-03-08 23:10:45 -0600

    Json data returned and error handling in place. State managed properly based on res status.

--- ninjablog/src/Home.js
+++ ninjablog/src/Home.js
@@ -4,26 +4,35 @@ import BlogList from './BlogList';
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
-    
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
             .then(res => {
+                console.log(res)
+                if(!res.ok) {
+                    throw Error('could not fetch the date for that resource')
+                }
                 return res.json();
             })
             .then(data => {
-                console.log(data);
                 setBlogs(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                setIsPending(false);
+                setError(err.message);
             })
     }, []);
+
         return (
             <div className="home">
-                {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>}
+                { error && <div>{ error }</div>}
+                { isPending && <div>Loading...</div>}
+                {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
             </div>
     );
 }
