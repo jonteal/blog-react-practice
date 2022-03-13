8a3c2f28524f12baeed6ecbd296ce4532b00840d
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-03-12 21:44:29 -0600

    404 page created. Basic project complete

--- ninjablog/src/App.js
+++ ninjablog/src/App.js
@@ -4,6 +4,7 @@ import Home from './Home';
 import Create from './Create';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 
 function App() {
@@ -23,6 +24,9 @@ function App() {
           </Route>
           <Route exact path='/blogs/:id'>
             <BlogDetails />
+          </Route>
+          <Route path="*">
+            <NotFound />
           </Route>
         </Switch>
       </div>
